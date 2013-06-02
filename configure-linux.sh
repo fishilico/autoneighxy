78d5ed74e8b35af0d5fd5a0fd16cf76b126a5a34
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2013-06-02 20:50:32 +0200

    Fix typos in README
    Thanks to marteo, who read the README file.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

--- configure-linux.sh
+++ configure-linux.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Configure kernel and firewall to accept what is needed
 
+# You may be more specific by replacing "all" by an interface
+echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 1 > /proc/sys/net/ipv6/conf/all/proxy_ndp
-#echo 2 > /proc/sys/net/ipv6/conf/all/forwarding
+echo 2 > /proc/sys/net/ipv6/conf/all/forwarding
 
 # Please use more specific rules if you know what network prefix your LAN uses
 iptables -A FORWARD -j ACCEPT
